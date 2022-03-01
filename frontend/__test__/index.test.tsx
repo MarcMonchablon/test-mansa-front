33bed6d4be66745882430d61263f7010009617a6
Marc Monchablon <marc.monchablon.git@gmail.com>
2022-03-01 18:43:10 +0100

    Index: remove default (failing) test

--- frontend/__test__/index.test.tsx
+++ frontend/__test__/index.test.tsx
@@ -5,13 +5,7 @@
 import React from 'react'
 
 describe('Home', () => {
-  // it('renders a heading', () => {
-  //   render(<Home />)
-
-  //   const heading = screen.getByRole('heading', {
-  //     name: /welcome to next\.js!/i,
-  //   })
-
-  //   expect(heading).toBeInTheDocument()
-  // })
-})
+  it('exists', () => {
+    // TODO
+  });
+});
