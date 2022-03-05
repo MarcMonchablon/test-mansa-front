83cbc60283f52a10231fa997ae5eaa220028cacb
Marc Monchablon <marc.monchablon.git@gmail.com>
2022-03-05 18:30:11 +0100

    Project: add Cypress E2E test framework

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -13,6 +13,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/services/(.*)$': '<rootDir>/services/$1',
